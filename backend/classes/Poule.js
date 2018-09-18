2bb6484eff4535f1cdc505038b159c8f7e5ec5c6
Haris Spahija <harisspahija@localhost.localdomain>
2018-09-19 01:50:30 +0200

    Added simulate poule functionality

--- backend/classes/Poule.js
+++ backend/classes/Poule.js
@@ -1,4 +1,6 @@
 const Team = require("./Team");
+const Match = require("./Match");
+
 
 class Poule {
   constructor() {
@@ -8,6 +10,22 @@ class Poule {
       psv: new Team("PSV", 100),
       peczwolle: new Team("PEC Zwolle", 90)
     };
+  }
+
+  simulatePoule() {
+    let match = new Match()
+
+    //Day 1
+    match.playMatch(this.teams.ajax, this.teams.feyenoord)
+    match.playMatch(this.teams.psv, this.teams.peczwolle)
+
+    //Day 2
+    match.playMatch(this.teams.ajax, this.teams.peczwolle)
+    match.playMatch(this.teams.psv, this.teams.feyenoord)
+
+    //Day 3
+    match.playMatch(this.teams.ajax, this.teams.psv)
+    match.playMatch(this.teams.peczwolle, this.teams.feyenoord)
   }
 
   pouleReset() {
