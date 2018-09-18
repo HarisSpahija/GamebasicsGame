5a8e6cab455b9e0a10e01b37adc8364bae054912
Haris Spahija <harisspahija@localhost.localdomain>
2018-09-19 01:13:52 +0200

    Minor bug fix on team.js Changed calcPower to calculate power Bonus with power.

--- backend/classes/Team.js
+++ backend/classes/Team.js
@@ -1,37 +1,38 @@
 class Team {
-    constructor(name, power) {
-        this.name = name;
-        this.power = power;
-        this.wins = 0;
-        this.loss = 0;
-        this.draws = 0;
-        this.points = 0;
-    }
+  constructor(name, power) {
+    this.name = name;
+    this.power = power;
+    this.wins = 0;
+    this.loss = 0;
+    this.draws = 0;
+    this.points = 0;
+  }
 
-    winMatch() {
-        this.points += 3;
-        this.wins++;
-    }
+  winMatch() {
+    this.points += 3;
+    this.wins++;
+  }
 
-    loseMatch() {
-        this.loss++;
-    }
+  loseMatch() {
+    this.loss++;
+  }
 
-    drawMatch() {
-        this.points += 1;
-        this.draws++;
-    }
+  drawMatch() {
+    this.points += 1;
+    this.draws++;
+  }
 
-    calcPowerBonus(){
-        return (powerBonus = Math.round(Math.random() * 10))
-    }
+  calcPower() {
+    let powerBonus = this.power + Math.round(Math.random() * 10);
+    return powerBonus;
+  }
 
-    resetScore() {
-        this.wins = 0;
-        this.loss = 0;
-        this.draws = 0;
-        this.points = 0;
-    }
+  resetScore() {
+    this.wins = 0;
+    this.loss = 0;
+    this.draws = 0;
+    this.points = 0;
+  }
 }
 
 module.exports = Team
